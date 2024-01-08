16d6b58840912d828eb9f5ab52bb6b4998c641cd
fbehddn <tom0611@naver.com>
2024-01-08 15:17:02 +0900

    hash - Q2 - 문제 풀이 완료 (리팩토링)

--- src/hash/Ponkemon.java
+++ src/hash/Ponkemon.java
@@ -1,6 +1,6 @@
 package hash;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Ponkemon {
     public static void main(String[] args) {
@@ -11,22 +11,13 @@ public class Ponkemon {
     }
     static class Solution {
         public int solution(int[] nums) {
-            HashMap<Integer, Integer> hm = new HashMap<>();
-            /**
-             * 중복된 숫자는 빼고 nums.length/2 만큼 뽑기
-             */
-            int idx = 0;
-            int count = 0;
-            while (true) {
-                // 포함하고 있지 않으면
-                if (!hm.containsKey(nums[idx])) {
-                    hm.put(nums[idx], nums[idx]);
-                    count++;
-                }
-                idx++;
-                if (count == nums.length / 2 || idx == nums.length) break;
+            HashSet<Integer> hs = new HashSet<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                hs.add(nums[i]);
             }
-            return hm.size();
+            if(hs.size()>nums.length/2) return nums.length / 2;
+            return hs.size();
         }
     }
 }
