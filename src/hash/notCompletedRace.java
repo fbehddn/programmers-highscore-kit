7c7e2e701cc2eedd2cecc2fd20893a445b556810
fbehddn <tom0611@naver.com>
2024-01-08 13:54:27 +0900

    hash - Q1 - 문제 풀이 완료

--- src/hash/notCompletedRace.java
+++ src/hash/notCompletedRace.java
@@ -1,13 +1,11 @@
 package hash;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
 
 public class notCompletedRace {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String result = solution.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"});
+        String result = solution.solution(new String[]{"leo"}, new String[]{});
 
         System.out.println(result);
     }
@@ -16,24 +14,24 @@ public class notCompletedRace {
 class Solution {
     public String solution(String[] participant, String[] completion) {
 
-        HashMap<Integer, String> mapP = new HashMap<>();
+        Arrays.sort(participant); // e k l // f j m n v  // a m m s
+        Arrays.sort(completion); // e k    // f j m n    // a m s
+
+        int count = 0;
+
+        if(completion.length==0) return participant[0];
 
         for (int i = 0; i < participant.length; i++) {
-            mapP.put(i, participant[i]);
-        }
-
-        for (int i = 0; i < completion.length; i++) {
-            for (int j = 0; j < participant.length; j++) {
-                if (completion[i].equals(mapP.get(j))) {
-                    mapP.remove(j);
-                    break;
-                }
+            if (participant[i].equals(completion[i])) {
+                count++;
+            }else{
+                return participant[i];
+            }
+            if (count == completion.length) {
+                return participant[participant.length - 1];
             }
         }
 
-        for (String value : mapP.values()) {
-            return value;
-        }
-        return null;
+        return "test";
     }
 }
