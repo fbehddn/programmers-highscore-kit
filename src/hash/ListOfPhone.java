b11b84ce1026464a2979f60f3d48697ac4d20c9d
fbehddn <tom0611@naver.com>
2024-01-09 14:46:06 +0900

    hash - Q3 - 문제 풀이 완료

--- src/hash/ListOfPhone.java
+++ src/hash/ListOfPhone.java
@@ -1,22 +1,30 @@
 package hash;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class ListOfPhone {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean result = solution.solution(new String[]{"123", "456", "789"});
+        boolean result = solution.solution(new String[]{"12", "123", "1235", "567", "88"});
 
         System.out.println(result);
     }
 
     static class Solution {
         public boolean solution(String[] phone_book) {
-            HashSet<String> hs = new HashSet<>();
-            for (String s : phone_book) {
-                hs.add(s.substring(0,1));
+            HashMap<Integer, String> hm = new HashMap<>();
+            Arrays.sort(phone_book); // 12 123 1235 567 88 => 무조건 뒤 인덱스가 앞 인덱스 값으로 시작하는지 판단할 수 있게 정렬
+
+            for (int i = 0; i < phone_book.length; i++) {
+                hm.put(i, phone_book[i]);
             }
-            return hs.size() == phone_book.length;
+
+            for (int i = 1; i < hm.size(); i++) {
+                if(hm.get(i).startsWith(hm.get(i-1))) return false;
+            }
+            return true;
         }
     }
 }
