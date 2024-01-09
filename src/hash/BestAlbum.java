4ad4338b21b07b0b0ca5c2e63bd5d385190dc8e7
fbehddn <tom0611@naver.com>
2024-01-09 22:09:07 +0900

    hash - Q5 - 문제 풀이 (내일다시)

--- src/hash/BestAlbum.java
+++ src/hash/BestAlbum.java
@@ -12,18 +12,44 @@ public class BestAlbum {
 
     static class Solution {
         public int[] solution(String[] genres, int[] plays) {
-            HashMap<String, Integer> hs = new HashMap<>();
-            HashMap<String, Integer> hs2 = new HashMap<>(); //원본
+            /**
+             * 가장 많이 들은 장르가 뭔지 구하는 로직만 짜면 끝
+             */
+            HashMap<String, Integer> temp = new HashMap<>();
+            HashMap<Integer, String> temp2 = new HashMap<>();
 
             for (int i = 0; i < genres.length; i++) {
-                hs.put(genres[i], hs.getOrDefault(genres[i], 0) + plays[i]);
-                hs2.put(genres[i], plays[i]);
+                temp.put(genres[i], temp.getOrDefault(genres[i], 0) + plays[i]);
+                temp2.put(temp.getOrDefault(genres[i], 0) + plays[i], genres[i]);
             }
-            // hs 의 value 가 큰 순서대로 key 를 담는 배열 생성
 
-            // 배열을 이용해 hs2에서 큰 값부터 인덱스번호 담기
+            HashMap<Integer, String> hm = new HashMap<>();
+            for (int i = 0; i < genres.length; i++) {
+                hm.put(plays[i], genres[i]);
+            }
+            /**
+             * classic 500 + 150 + 800
+             * pop 600 + 2500
+             * <classic, 1450>
+             * <pop, 3100>
+             *
+             * pop 장르의 values가 더 크다는 로직...
+             */
+            int max = 0;
+            String topPlayGenres = null;
+            for (String s : temp.keySet()) {
+                max = temp.get(s) > max ? temp.get(s) : max;
+            }
+            topPlayGenres = temp2.get(max);
 
-            return null;
+            int[] result = new int[temp.size() * 2];
+            int idx = 0;
+            for (Integer integer : temp2.keySet()) {
+                if (temp2.get(integer).equals(topPlayGenres)) {
+                    result[idx++] =
+                }
+            }
+
         }
     }
 }
