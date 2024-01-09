648af646f22d3a4057fb3bed2274416494ca1858
fbehddn <tom0611@naver.com>
2024-01-09 20:21:11 +0900

    hash - Q4 - 문제 풀이 완료

--- src/hash/Clothes.java
+++ src/hash/Clothes.java
@@ -11,38 +11,21 @@ public class Clothes {
         System.out.println(result);
     }
 
-    /**
-     * 총 개수 더한거에다가 + 곱한거(다른종류 의상끼리)
-     * 상세하게 무슨 정보인지는 알 필요가 없음 . 그냥 같은 종류 몇 개인지 개수만 count
-     */
-
     static class Solution {
         public int solution(String[][] clothes) {
             HashMap<String, Integer> hs = new HashMap<>();
-            // <headgear,2> , <eyewear,1> => (2+1) + (2*1) = 5
-            // <face,3> => (3) + (3*0) = 3
+
             for (String[] clothe : clothes) {
                 String type = clothe[1];
                 hs.put(type, hs.getOrDefault(type, 0) + 1);
             }
-            int sum = 0;
-            int mul;
-            // type이 1개 일 경우 = 0
-            // type이 2개 이상 일 경우 = 1
-
-            if (hs.size() == 1) {
-                mul = 0;
-            } else {
-                mul = 1;
-            }
+            int mul = 1;
 
             for (Integer value : hs.values()) {
-                sum += value;
-                mul *= value;
+                mul *= (value + 1);
             }
 
-            System.out.println("hs.size() = " + hs.size());
-            return sum + mul;
+            return mul - 1;
         }
     }
 }
