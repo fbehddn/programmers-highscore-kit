ac273e53944cbdb6137b8befd92b57f15ce9c362
fbehddn <tom0611@naver.com>
2024-01-11 13:29:03 +0900

    stack/queue - Q2 - 문제 풀이 (사용한 일수 사용)

--- src/stackORqueue/FunctionDev.java
+++ src/stackORqueue/FunctionDev.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class FunctionDev {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] result = solution.solution(new int[]{93, 30, 55}, new int[]{1, 30, 5});
+        int[] result = solution.solution(new int[]{95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1});
 
         for (int i : result) {
             System.out.print(i+" ");
@@ -17,24 +17,48 @@ public class FunctionDev {
             int[] result = new int[progresses.length];
             Stack<Integer> stk = new Stack<>();
 
-            int count = 1;
+            int[] useDay = new int[progresses.length];
+
+            /**
+             * pro 별로 speeds 를 계산하여 필요한 기간을 저장
+             */
+            // useDay = [5, 10, 1, 1, 20, 1]
+            // useDay = [0, 5, 0, 0, 15, 0] stk.size = 1 => return 1
+            // useDay = [0, 0, 0, 0, 10, 0] stk.size = 3 => return 3
+            // useDay = [0, 0, 0, 0, 0, 0] stk.size = 2 => return 2
+            // => [1,3,2]
+
             for (int i = 0; i < progresses.length; i++) {
-                int temp = progresses[i];
-                while(temp <100) {
-                    temp += speeds[i];
+                while(progresses[i] <100) {
+                    progresses[i] += speeds[i];
+                    useDay[i]++;
+                }
+            }
+
+            for (int i = 0; i < useDay.length; i++) {
+                int count = 0;
+
+                while(useDay[i]<=0) {
+                    useDay[i]--;
                     count++;
                 }
-                stk.push(progresses[i]);
 
-                if(i == progresses.length-1) break;
-
-                for (int j = 0; j < count; j++) {
-                    int temp2 = progresses[i+1];
-                    temp2 += speeds[i + 1];
-                    if(temp2>=100 && stk.peek() == progresses[i]) stk.push(progresses[i + 1]);
+                for (int j = 1; j < useDay.length; j++) {
+                    for (int k = 0; k < count; k++) {
+                     useDay[j]--;
+                    }
                 }
-                result[i] += stk.size();
 
+                stk.push(progresses[i]);
+                for (int z = 1; z < useDay.length; z++) {
+                    if(useDay[z] <=0 && stk.peek() == progresses[z-1]) {
+                        stk.push(progresses[z]);
+                    }
+                }
+                result[i] = stk.size();
+                for (int a = 0; a < stk.size(); a++) {
+                    stk.pop();
+                }
             }
             return result;
         }
