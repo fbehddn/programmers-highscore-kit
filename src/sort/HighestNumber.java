5398caa2957b498ad459f8136358a751832344a6
fbehddn <tom0611@naver.com>
2024-01-12 19:54:31 +0900

    sort - Q2 - 문제 풀이 시작

---
+++ src/sort/HighestNumber.java
@@ -0,0 +1,26 @@
+package sort;
+
+public class HighestNumber {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result = solution.solution(new int[]{6, 10, 2});
+
+        System.out.println(result);
+    }
+
+    static class Solution{
+        public String solution(int[] numbers) {
+            String answer = "";
+
+            /**
+             * 가장 큰 자리수를 기준으로 큰 수부터 정렬
+             */
+
+            for (int number : numbers) {
+
+            }
+
+            return answer;
+        }
+    }
+}
