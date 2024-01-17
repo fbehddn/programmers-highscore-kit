e19cd52570a3dddeae952cd00247de1e8dfa1686
fbehddn <tom0611@naver.com>
2024-01-17 13:51:53 +0900

    sort - Q2 - 엄청난 발상인줄 알았지만 문제 의도와 달랐던 것

--- src/sort/HighestNumber.java
+++ src/sort/HighestNumber.java
@@ -1,5 +1,8 @@
 package sort;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class HighestNumber {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -14,12 +17,32 @@ public class HighestNumber {
 
             /**
              * 가장 큰 자리수를 기준으로 큰 수부터 정렬
+             *
+             * 와 레전드 발상
+             * 그냥 각각의 자리수 모두 일차 배열에 담고 내림차순 정렬해서 출력하면 ?
              */
 
-            for (int number : numbers) {
+            ArrayList<String> temp = new ArrayList<>();
 
+            for (int number : numbers) {
+                temp.add(Integer.toString(number));
+            }
+            // temp[0] = 6, temp[1] = 10, temp[2] = 2
+
+            ArrayList<Integer> intList = new ArrayList<>();
+            int count = 0;
+            for (String s : temp) {
+                for (int i = 0; i < s.length(); i++) {
+                    intList.add(Integer.parseInt(Character.toString(s.charAt(i))));
+                }
             }
 
+            //intArr[0] = 6, intArr[1] = 1, intArr[2] = 0, intArr[3] = 2
+
+            Collections.sort(intList);
+            for (int i = intList.size() - 1; i >= 0; i--) {
+                answer += Integer.toString(intList.get(i));
+            }
             return answer;
         }
     }
