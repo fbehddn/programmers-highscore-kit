43540fe728b22e72f0789a09175661b968dd6dc7
fbehddn <tom0611@naver.com>
2024-03-06 18:58:07 +0900

    sort - Q2 - 문제 풀이 완료

--- src/sort/HighestNumber.java
+++ src/sort/HighestNumber.java
@@ -1,7 +1,6 @@
 package sort;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class HighestNumber {
     public static void main(String[] args) {
@@ -13,37 +12,23 @@ public class HighestNumber {
 
     static class Solution{
         public String solution(int[] numbers) {
-            String answer = "";
-
             /**
-             * 가장 큰 자리수를 기준으로 큰 수부터 정렬
-             *
-             * 와 레전드 발상
-             * 그냥 각각의 자리수 모두 일차 배열에 담고 내림차순 정렬해서 출력하면 ?
+             * int idx = 0
+             * charAt[idx] 가장 큰 배열원소 List 에 추가
+             * 만약 3, 30, 34 로 charAt[0] 값이 같으면 idx++ 값이 큰 순서대로 List 에 추가
+             * 이때 330 > 303 이므로 "자리수가 존재하지 않는 값 > 자리수가 0인 값" 으로 판정
              */
 
-            ArrayList<String> temp = new ArrayList<>();
-
-            for (int number : numbers) {
-                temp.add(Integer.toString(number));
-            }
-            // temp[0] = 6, temp[1] = 10, temp[2] = 2
-
-            ArrayList<Integer> intList = new ArrayList<>();
-            int count = 0;
-            for (String s : temp) {
-                for (int i = 0; i < s.length(); i++) {
-                    intList.add(Integer.parseInt(Character.toString(s.charAt(i))));
-                }
+            String[] strArr = new String[numbers.length];
+            for (int i = 0; i < strArr.length; i++) {
+                strArr[i] = String.valueOf(numbers[i]);
             }
 
-            //intArr[0] = 6, intArr[1] = 1, intArr[2] = 0, intArr[3] = 2
+            Arrays.sort(strArr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
 
-            Collections.sort(intList);
-            for (int i = intList.size() - 1; i >= 0; i--) {
-                answer += Integer.toString(intList.get(i));
-            }
-            return answer;
+            if(strArr[0].equals("0")) return "0";
+
+            return String.join("", strArr);
         }
     }
 }
